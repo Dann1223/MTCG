08b7804ff0fc6728a6ebf0c5b592651a94fdf436
dan gu <if23b291@technikum-wien.at>
2024-09-30 00:25:13 +0200

    update

--- game/Program.cs
+++ game/Program.cs
@@ -40,5 +40,38 @@ namespace game
         response.OutputStream.Close();
       }
     }
+
+
+    static string HandleRegister(HttpListenerRequest request)
+    {
+      try
+      {
+        // 输出请求的 HTTP 方法和 URL 路径
+        Console.WriteLine($"Received {request.HttpMethod} request for {request.Url.AbsolutePath}");
+
+        // 读取请求体
+        var body = new System.IO.StreamReader(request.InputStream).ReadToEnd();
+        Console.WriteLine($"Request body: {body}"); // 输出请求体
+
+        var registerData = JsonConvert.DeserializeObject<RegisterRequest>(body);
+      }
+      catch
+      {
+
+      }
+    }
+  }
+
+
+  public class User
+  {
+    public string Username { get; set; }
+    public string Password { get; set; }
+  }
+
+  public class RegisterRequest
+  {
+    public string Username { get; set; }
+    public string Password { get; set; }
   }
 }
